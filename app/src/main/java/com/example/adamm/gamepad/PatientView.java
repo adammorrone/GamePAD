22d7a2464618e627bd37cef0c7be0c78a1bfc391
ernestsco <ernestsco@gmail.com>
2018-02-16 14:12:53 -0500

    changing patientView layout

--- app/src/main/java/com/example/adamm/gamepad/PatientView.java
+++ app/src/main/java/com/example/adamm/gamepad/PatientView.java
@@ -166,7 +166,6 @@ public class PatientView extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        int lovelyBones = 0;
         //Get MAC address from DeviceListActivity via intent
         //Intent intent = getIntent();
 

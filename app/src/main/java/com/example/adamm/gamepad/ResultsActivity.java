fc1d2c079b3067e6920ccdc4cb38b2b63cc224a4
ernestsco <ernestsco@gmail.com>
2018-03-20 13:59:42 -0400

    added start button to landscape layout Disabled save functions in resultsView (causing error) Changed InfoView to TextView to keep keyboard from popping up on activity create.

--- app/src/main/java/com/example/adamm/gamepad/ResultsActivity.java
+++ app/src/main/java/com/example/adamm/gamepad/ResultsActivity.java
@@ -28,7 +28,9 @@ public class ResultsActivity extends AppCompatActivity {
     private PatientList masterList = MainActivity.masterList;
     public  TextView scoreText;
     public  TextView highscoreLabel;
-    public EditText infoText;
+    //public EditText infoText;
+    //changed editText to textview
+    public TextView infoText;
 
 
     @Override
@@ -53,11 +55,11 @@ public class ResultsActivity extends AppCompatActivity {
         index = (int)b.get("Patient");
         int _score_ = (int)b.get("Score");
         int _throws_ = (int)b.get("Throws");
-        int _weight_ = Integer.parseInt((String)b.get("Weight"));
+        //int _weight_ = Integer.parseInt((String)b.get("Weight"));
 
-        //String weight = b.get("Weight").toString();
-        //Scanner in = new Scanner(weight).useDelimiter("[^0-9]+");
-        //int _weight_ = in.nextInt();
+        String weight = b.get("Weight").toString();
+        Scanner in = new Scanner(weight).useDelimiter("[^0-9]+");
+        int _weight_ = in.nextInt();
         double _distance_ = Double.parseDouble(((String)b.get("Distance")).substring(0, 2));
         int _time_ = Integer.parseInt(((String)b.get("Time")).substring(0, 1));
         int _height_ = masterList.getPatient(index).getHeight();
@@ -78,7 +80,7 @@ public class ResultsActivity extends AppCompatActivity {
         scoreText.setText(Double.toString(scoreRecord.getScore()));
         //infoText.setText(scoreRecord.toString());
         //temp
-        infoText.setText("NULL");
+        //infoText.setText("NULL");
 
 
 

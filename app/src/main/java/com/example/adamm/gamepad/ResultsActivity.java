c561d030e740398d265c6c7b56428e86c7d133bd
ernestsco <ernestso@gmail.com>
2018-03-21 18:05:05 -0400

    Scanner added to parsing data from previous intent

--- app/src/main/java/com/example/adamm/gamepad/ResultsActivity.java
+++ app/src/main/java/com/example/adamm/gamepad/ResultsActivity.java
@@ -56,12 +56,22 @@ public class ResultsActivity extends AppCompatActivity {
         int _score_ = (int)b.get("Score");
         int _throws_ = (int)b.get("Throws");
         //int _weight_ = Integer.parseInt((String)b.get("Weight"));
+        //double _distance_ = Double.parseDouble(((String)b.get("Distance")).substring(0, 2));
+        //int _time_ = Integer.parseInt(((String)b.get("Time")).substring(0, 1));
 
+        // Parsing int with Scanner class
         String weight = b.get("Weight").toString();
         Scanner in = new Scanner(weight).useDelimiter("[^0-9]+");
         int _weight_ = in.nextInt();
-        double _distance_ = Double.parseDouble(((String)b.get("Distance")).substring(0, 2));
-        int _time_ = Integer.parseInt(((String)b.get("Time")).substring(0, 1));
+
+        String distance = b.get("Distance").toString();
+        in = new Scanner(distance).useDelimiter("[^0-9]+");
+        double _distance_ = in.nextInt();
+
+        String time = b.get("Time").toString();
+        in = new Scanner(time).useDelimiter("[^0-9]+");
+        int _time_ = in.nextInt();
+
         int _height_ = masterList.getPatient(index).getHeight();
 
         ScoreRecord scoreRecord = new ScoreRecord(_score_, "Standard", Calendar.getInstance(), _distance_, _throws_, _time_, _weight_, _height_);
